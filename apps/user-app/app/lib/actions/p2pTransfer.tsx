8a6c13c33662cc269ab6740f8b91f0f8a2b3e4a6
Harsh Arora <harshpunyani20@gmail.com>
2026-02-06 22:55:28 +0530

    fix_p2p_txn

--- apps/user-app/app/lib/actions/p2pTransfer.tsx
+++ apps/user-app/app/lib/actions/p2pTransfer.tsx
@@ -2,6 +2,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth";
 import db from "@repo/db"
+import { error } from "console";
  
 export async function p2pTransfer(to: string, amount: number) {
     const session = getServerSession(authOptions);
@@ -23,16 +24,26 @@ export async function p2pTransfer(to: string, amount: number) {
         }
     }
     await db.$transaction(async (txn) => {
+        //locking
+         await txn.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
+
           const fromBalance = await txn.balance.findUnique({
             where: {
                 userId: Number(from)
-            },
-            data: {amount : {decrement: amount}},
+            }
           });
+          if(!fromBalance || fromBalance.amount < amount) {
+            throw new Error("Insufficent Balance");
+          }
           await txn.balance.update({
-            where: {userId: toUser.id},
-            data: {amount: {increment: amount}},
-          })
+            where: {userId: Number(from)},
+            data: {amount: {decrement: amount}},
+          });
+     await txn.balance.update({
+        where : {userId: toUser.id},
+        data: {amount: {increment: amount}}
+     })
+
     })
 
 }
